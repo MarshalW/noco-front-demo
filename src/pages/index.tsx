69449c2f5c9532c23a59350ada4bc28f2438c623
Marshal Wu <marshal.wu@gmail.com>
2022-05-05 19:36:13 +0800

    最简单的静态switch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
-const App=()=>{
+import { MemoryRouter as Router, Link, Switch, Route } from 'react-router-dom';
+
+const Home = () => <h1>Home</h1>;
+const About = () => <h1>About</h1>;
+
+const App = () => {
   return (
-    <div>Hello~~</div>
-  )
-}
+    <Router>
+      <Link to="/">首页</Link>
+      <Link to="/about">关于</Link>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
