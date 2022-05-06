4795d6668363db5dcd561b2147c1fc0937d27828
Marshal Wu <marshal.wu@gmail.com>
2022-05-06 09:23:33 +0800

    紧耦合的routeSwitch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,46 @@ import { MemoryRouter as Router, Link, Switch, Route } from 'react-router-dom';
 const Home = () => <h1>Home</h1>;
 const About = () => <h1>About</h1>;
 
+const routes: object[] = [
+  {
+    path: '/',
+    exact: true,
+    component: Home,
+  },
+  {
+    path: '/about',
+    component: About,
+  },
+];
+
+function RouteSwitch(props: any) {
+  const { routes = [] } = props;
+  if (!routes.length) {
+    return null;
+  }
+
+  return (
+    <Switch>
+      {routes.map((route: any, index: number) => {
+        return (
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            render={route.component}
+          />
+        );
+      })}
+    </Switch>
+  );
+}
+
 const App = () => {
   return (
     <Router>
       <Link to="/">首页</Link>
       <Link to="/about">关于</Link>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-      </Switch>
+      <RouteSwitch routes={routes} />
     </Router>
   );
 };
